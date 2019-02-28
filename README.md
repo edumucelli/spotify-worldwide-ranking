41bb55d1a2646d16834cb74d80aaa2e733185bc2
Dibad <dibadgithub@gmail.com>
2019-02-28 00:40:29 +0000

    Changes to work with Python3 and current spotify webpage

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # spotify-worldwide-ranking
 Automate data collection from Spotify's worldwide ranking in 50+ countries. The snapshot of the data collected by the code has been published at [Kaggle datasets](https://www.kaggle.com/edumucelli/spotifys-worldwide-daily-song-ranking).
 
+
 ### Usage
 
 Install the required packages and run the script by
 ```
-pip install -r requirements.txt
-python spotify.py
+pip3 install -r requirements.txt
+python3 spotify.py
 ```
 
 The collected data will be stored on the `data` directory.
--- spotify.py
+++ spotify.py
@@ -1,5 +1,6 @@
 # Author: Eduardo Mucelli Rezende Oliveira
 # E-mail: edumucelli@gmail.com
+# Modified for working with Python 3 at 2019-02-28: David Afonso Dorta
 
 import os
 from datetime import datetime, timedelta, date
@@ -9,7 +10,7 @@ import threading
 from tqdm import tqdm
 import requests
 
-DATA_DIRECTORY = 'data'
+DATA_DIR = 'data'
 
 
 class Collector(threading.Thread):
@@ -18,7 +19,8 @@ class Collector(threading.Thread):
         self.region = region
         self.start_date = start_date
         self.end_date = end_date
-        self.base_headers = ['Position', 'Track Name', 'Artist', 'Streams', 'URL']
+        self.base_headers = ['Position',
+                             'Track Name', 'Artist', 'Streams', 'URL']
 
     def date_range(self):
         one_day = timedelta(days=1)
@@ -29,53 +31,44 @@ class Collector(threading.Thread):
 
     def is_csv_ok(self, download_content):
         csv_reader = csv.reader(download_content.splitlines(), delimiter=',')
-        headers = csv_reader.next()
+        headers = next(csv_reader)
         return set(headers) == set(self.base_headers)
 
     def download_csv_file(self, url):
         with requests.Session() as session:
-            session.headers.update({'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                                    'Accept-Encoding': 'gzip, deflate',
-                                    'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
-                                    'Connection': 'keep-alive',
-                                    # 'Cookie': 'X-Mapping-jfgkgcmo=C2ECD7B19B260AB2D849DAB632033510; laravel_session=eyJpdiI6ImZFN2lLOEFNVmhndDFrUDc5QmVrdXc9PSIsInZhbHVlIjoiU21zYTFpQUljaTF1cmJnN1kzVm5mbVFJZldzTzQ5ZGZIWWNpZnJKRU1ubkZlU1FBOTRxSGpiaXljMGVcL3NObGF4Zm5VUjllQ0Yxb0R2RnJveXVDcktBPT0iLCJtYWMiOiI4MjY5MTdmMGMzNWIwMDU3OGYyY2Q0MjZlZmEzNmM0NjNlOGU1MTk0NmNjODU0M2I3MTc1NjEwNTJiZTEzOTQyIn0%3D; _ga=GA1.2.852419413.1501944945; _gid=GA1.2.288555599.1501944945; XSRF-TOKEN=eyJpdiI6IjNCY2tEakUwb28ycFVCUDFrYWtxTGc9PSIsInZhbHVlIjoiR05vS0lhalQ1WEtiejBGd1Z2cnZaREdlczlcLzNNdU1UdjI2b1FReGN6cFZEV1pnbmVlVHRURU9JVm40MDlmcDRpVTFmaGtQbG41UElcL0NJdjJueVlwdz09IiwibWFjIjoiNTcyOTI2NDczZjhiNjRkYTYxYzM0N2Y2ZGVmMWU4YjI0NTI3MDk4OThiNTkwNThiNDc0ODRiMTU4MjNjOTRhNyJ9; 9725eef18a75e1784d10b0949938c4d45f081300=eyJpdiI6InBicWZMQlRtZUoycUlKd0ZSQ05SM3c9PSIsInZhbHVlIjoib2o1VFJGSTdhakhtVDhyXC9xWVVuN0RobzhNOVBVT2crRmg0NFNrNGhTcEI4XC9qSEpuYUliVGdOZXZMU3JxRWxoS1d3UmpBRkRhR0EwMHRwSDE5MVA3U3dNbEZXSER6RHErTmlcL2tvbFZHNTN5XC9tQjBSb0JuQnFMdUJkZzNDTlZLeUdqeDRYaGRJXC92VG5qVTVyWkgxSGlweWlIM1JNbzBSSDZwQ0xXS1ZuUEZJTElpa3pPYnNOS1JSYXFCUXQ1MDduZVl5VHZhczJLaUNIVzVWYnMyWlwvQW1WdUJ3MWU3ek1pOU5ocEFYYjY4eUlabTNUWFUyWnRaVTFaZ0RWN0lYRm0zb0V4K2R4YklcLzdPdUY3eFg4elNKTGNSVDd5Skhicm1JQUN3NCt1MmQwVlhcL0xuOFVcL1lpMHk3bG1iTmFzWVN4dlU1Rk54dDJqOVdkSkozcmt1WU1CN3NjQm9KVnVLUmQxS1cxaGxtenhtNlljSmFaclNoeHROU1hWaCtNSGViZFRXcTJMallcL3ljNnpmNlJwRUJTWEE9PSIsIm1hYyI6Ijk1YWQ5NmVlYWI4MDA3ZTMwZTYyMjc3NjY5NDNmMzI5NjE0YmE5NzU2NDE0MzRjNDNmNjFjN2NhNmYwNzYzYWIifQ%3D%3D; _gat=1',
-                                    'Host': 'spotifycharts.com',
-                                    'Referer': 'https://spotifycharts.com/regional/ad/weekly/latest',
-                                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'})
-            retry = 3
-            while True:
-                download = session.get(url)
-                if self.is_csv_ok(download.content):
-                    return download.content
-                print "Retrying for '%s'" % url
-                retry -= 1
-                if retry <= 0:
-                    print "Retry failed for '%s'" % url
-                    return None
+            download = session.get(url)
+
+            return download.content.decode('utf-8')
 
     def extract_csv_rows(self, csv_file):
         csv_reader = csv.reader(csv_file.splitlines(), delimiter=',')
-        # Skip headers
-        csv_reader.next()
+        # Skip headers (for some reason there's a blank line after headers)
+        next(csv_reader)
+        next(csv_reader)
         for row in csv_reader:
             yield row
 
     def run(self):
-        if not os.path.exists(DATA_DIRECTORY):
-            os.makedirs(DATA_DIRECTORY)
+        if not os.path.exists(DATA_DIR):
+            os.makedirs(DATA_DIR)
 
         headers = self.base_headers + ["Date", "Region"]
 
-        file_path = os.path.join(DATA_DIRECTORY, "%s.csv" % self.region)
+        file_path = os.path.join(DATA_DIR, "%s.csv" % self.region)
         if os.path.exists(file_path):
-            print "File '%s' already exists, skipping" % file_path
+            print("File '%s' already exists, skipping" % file_path)
 
-        with open(file_path, 'wb', 1) as out_csv_file:
+        with open(file_path, 'w', 1) as out_csv_file:
             writer = csv.writer(out_csv_file)
             writer.writerow(headers)
 
-            for current_date in tqdm(self.date_range(), desc="Collecting from '%s'" % self.region):
-                url = "https://spotifycharts.com/regional/%s/daily/%s/download" % (self.region, current_date)
+            for current_date in tqdm(self.date_range(),
+                                     desc="Collecting from '%s'" %
+                                     self.region):
+
+                url = "https://spotifycharts.com/regional/ \
+                        %s/daily/%s/download" % (self.region, current_date)
+
                 csv_file = self.download_csv_file(url)
                 if csv_file is None:
                     continue
@@ -90,12 +83,16 @@ class Collector(threading.Thread):
 
         with open(final_filename, 'w') as outfile:
             csv_writer = csv.writer(outfile)
-            csv_writer.writerow(['Position', 'Track Name', 'Artist', 'Streams', 'URL'])
-            for filename in tqdm(os.listdir(DATA_DIRECTORY), desc="Generating final file: %s" % final_filename):
+            csv_writer.writerow(
+                ['Position', 'Track Name', 'Artist', 'Streams', 'URL'])
+            for filename in tqdm(os.listdir(DATA_DIR),
+                                 desc="Generating final file: %s" %
+                                 final_filename):
+
                 if filename.endswith(".csv"):
-                    with open(os.path.join(DATA_DIRECTORY, filename)) as infile:
+                    with open(os.path.join(DATA_DIR, filename)) as infile:
                         csv_reader = csv.reader(infile)
-                        csv_reader.next()
+                        next(csv_reader)
                         for row in csv_reader:
                             csv_writer.writerow(row)
 
@@ -111,9 +108,16 @@ if __name__ == "__main__":
                "dk", "do", "ec", "ee", "es", "fi", "fr", "gr", "gt", "hk",
                "hn", "hu", "id", "ie", "is", "it", "jp", "lt", "lu", "lv",
                "mc", "mt", "mx", "my", "ni", "nl", "no", "nz", "pa", "pe",
-               "ph", "pl", "pt", "py", "se", "sg", "sk", "sv", "tr", "tw", "uy"]
+               "ph", "pl", "pt", "py", "se", "sg", "sk", "sv", "tr", "tw",
+               "uy"]
 
+    threads = []
     for region in regions:
         collector = Collector(region, start_date, end_date)
         collector.start()
+        threads.append(collector)
+
+    for thread in threads:
+        thread.join()
+
     Collector.generate_final_file()
