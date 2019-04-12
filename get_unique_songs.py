5f9e1d1f260d448f48cde215084d45edaa31c789
Dibad <dibadgithub@gmail.com>
2019-04-12 16:34:46 +0100

    Positions are now from 100 (best) to 0

--- get_unique_songs.py
+++ get_unique_songs.py
@@ -5,6 +5,7 @@ import os
 from tqdm import tqdm
 
 NEW_HEADERS = ["top_pos", "pos_area", "pos_each_day"]
+WORST_POS = 101
 
 
 def get_unique_songs(input_filepath):
@@ -27,35 +28,35 @@ def get_unique_songs(input_filepath):
         top_pos_index = header.index("top_pos")
         pos_each_day_index = header.index("pos_each_day")
         pos_area_index = header.index("pos_area")
+        url_index = header.index("URL")
+
+        # Iterating variables
+        last_day = 1
+        iter_day = 1
 
         songs_dict = {}
 
         # First Iteration: Get unique values
         for row in tqdm(csv.reader(lines), total=len(lines)):
             # URL value
-            url = row[header.index("URL")]
+            url = row[url_index]
 
             # Use the url of the song as the key and add if not already in dict
             if url not in songs_dict:
-                row.append([])  # TODO: Make better
-                row.append([])
-                row.append([])
+                row.extend([[]] * len(NEW_HEADERS))
                 songs_dict[url] = row
-
-        last_day = 1
-        iter_day = 1
 
         # Second Iteration: Track the position value for each song each day
         for row in tqdm(csv.reader(lines), total=len(lines)):
             # Current day value
             current_day = int(row[header.index("Day")])
             # URL value
-            url = row[header.index("URL")]
+            url = row[url_index]
             # Posiiton value
             pos = int(row[header.index("Position")])
 
             if url in songs_dict:
-                songs_dict[url][pos_each_day_index].append(pos)
+                songs_dict[url][pos_each_day_index].append(WORST_POS - pos)
 
             if current_day != last_day:
                 last_day = current_day
@@ -69,9 +70,7 @@ def get_unique_songs(input_filepath):
         # Third iteration: Calculate the area for each song and the top pos
         for song_row in tqdm(songs_dict.values(), total=len(songs_dict)):
             # Get song area
-            song_row[pos_area_index] = sum(
-                map(lambda x: 0 if x == 0 else 101 - x,
-                    song_row[pos_each_day_index]))
+            song_row[pos_area_index] = sum(song_row[pos_each_day_index])
 
             # Get top pos
             song_row[top_pos_index] = max(song_row[pos_each_day_index])
