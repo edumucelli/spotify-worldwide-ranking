942ffc30f016a068ff11e0c1d06f555ccd07f098
Dibad <dibadgithub@gmail.com>
2019-04-12 16:16:34 +0100

    Calculate song position array, area and top pos

--- get_unique_songs.py
+++ get_unique_songs.py
@@ -4,7 +4,8 @@ import os
 
 from tqdm import tqdm
 
-NEW_HEADERS = ["pos_each_day"]
+NEW_HEADERS = ["top_pos", "pos_area", "pos_each_day"]
+
 
 def get_unique_songs(input_filepath):
     region = os.path.splitext(os.path.basename(input_filepath))[0]
@@ -23,7 +24,9 @@ def get_unique_songs(input_filepath):
         lines = [line for line in csv_input]
 
         # Indices
+        top_pos_index = header.index("top_pos")
         pos_each_day_index = header.index("pos_each_day")
+        pos_area_index = header.index("pos_area")
 
         songs_dict = {}
 
@@ -34,40 +37,51 @@ def get_unique_songs(input_filepath):
 
             # Use the url of the song as the key and add if not already in dict
             if url not in songs_dict:
+                row.append([])  # TODO: Make better
+                row.append([])
                 row.append([])
                 songs_dict[url] = row
 
-
         last_day = 1
-        start_year = 2017
+        iter_day = 1
 
-        # TODO: FINISH. NOT WORKING. WIP
-        # Second Iteration: Count positions for each day
+        # Second Iteration: Track the position value for each song each day
         for row in tqdm(csv.reader(lines), total=len(lines)):
+            # Current day value
             current_day = int(row[header.index("Day")])
-
             # URL value
             url = row[header.index("URL")]
-
             # Posiiton value
-            pos = row[header.index("Position")]
+            pos = int(row[header.index("Position")])
 
             if url in songs_dict:
                 songs_dict[url][pos_each_day_index].append(pos)
 
             if current_day != last_day:
                 last_day = current_day
-                month = int(row[header.index("Month")])
-                year = int(row[header.index("Year")])
-
-                year_diff = year - start_year + 1
 
                 for song_row in songs_dict.values():
-                    if(len(song_row[pos_each_day_index]) < last_day * month * year_diff):
+                    if (len(song_row[pos_each_day_index]) < iter_day):
                         song_row[pos_each_day_index].append(0)
 
-        for song_row in songs_dict.values():
-            writer.writerow(song_row);
+                iter_day += 1
+
+        # Third iteration: Calculate the area for each song and the top pos
+        for song_row in tqdm(songs_dict.values(), total=len(songs_dict)):
+            # Get song area
+            song_row[pos_area_index] = sum(
+                map(lambda x: 0 if x == 0 else 101 - x,
+                    song_row[pos_each_day_index]))
+
+            # Get top pos
+            song_row[top_pos_index] = max(song_row[pos_each_day_index])
+
+        # Write the results to the output .csv
+        for song_row in tqdm(songs_dict.values(), total=len(songs_dict)):
+            if (len(song_row[pos_each_day_index]) < iter_day):
+                song_row[pos_each_day_index].append(0)
+
+            writer.writerow(song_row)
 
 
 if __name__ == "__main__":
